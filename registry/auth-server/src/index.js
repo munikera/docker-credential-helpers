5a7f43a94560102acb03e8b0b1d6c1ef33ccabc7
munikera <109246308+munikera@users.noreply.github.com>
2022-12-01 15:01:35 -0800

    remove 401 call

--- registry/auth-server/src/index.js
+++ registry/auth-server/src/index.js
@@ -5,10 +5,6 @@ const port = 8000;
 app.get("/", (req, res) => {
   console.log("my headers:", req.headers);
 
-  if (!req.headers.authorization) {
-    return res.status(401).send();
-  }
-
   const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
   const [login, password] = Buffer.from(b64auth, "base64")
     .toString()
